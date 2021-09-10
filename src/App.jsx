c3cf9a4617bb10a9635d07706e8ddd5562f99178
JennyChiang <chihyueh871125@gmail.com>
2021-09-10 10:21:14 +0800

    Initial webpack development environment

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const App = () => {
+    return <h1>My First Webpack React App </h1>;
+}
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        path:  path.resolve(__dirname, 'build/'),
+        filename: 'bundle.js',
+        publicPath: '/',
+        clean: true,
+    },
+    module: {
+        rules:  [
+            {
+                test: /\.(js|jsx)?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    devServer: {
+        client: {
+            logging: 'info',
+            overlay: true,
+        },
+        static: {
+            directory: path.join(__dirname, 'build'),
+            watch: true,
+        },
+        hot: true,
+        open: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+};
