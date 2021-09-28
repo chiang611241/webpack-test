33462c27d2ad706acf6b5f39821a78ddf5e96c62
JennyChiang <chihyueh871125@gmail.com>
2021-09-28 11:17:47 +0800

    Add test button in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => (
-  <h1>My First Webpack React App </h1>
-);
+const App = () => {
+  const [num, setNum] = useState(0);
+  const onChange = () => {
+    setNum(num + 1);
+  };
+  return (
+    <div>
+      <p>{num}</p>
+      <button onClick={onChange} type="button">Add</button>
+    </div>
+  );
+};
 
 export default App;
